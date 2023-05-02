db673a2ce40578d620dc9d531db545967898eb86
DennisAlejandro99 <alejandroibarrapr99@gmail.com>
2023-05-01 20:34:19 -0500

    initial commit

---
+++ Aplicación_web_test_1-29-4-23/Aplicación_web_test_1-29-4-23/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Aplicación_web_test_1_29_4_23.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
